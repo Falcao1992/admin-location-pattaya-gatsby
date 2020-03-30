d5179aad7294dbac18640962e541f533eb3d4cc4
Falcao1992 <eduardo.marselle@gmail.com>
2020-03-30 02:17:58 +0200

    add and configure .env file

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import * as firebase from "firebase";
 import "firebase/auth";
 
 const app = firebase.initializeApp({
-    apiKey: "AIzaSyCOj-sa7CxJHpuLFpg8-YThcNqvjvjnsAE",
-    authDomain: "location-pattaya.firebaseapp.com",
-    databaseURL: "https://location-pattaya.firebaseio.com",
-    projectId: "location-pattaya",
-    storageBucket: "location-pattaya.appspot.com",
-    messagingSenderId: "1040793840778",
-    appId: "1:1040793840778:web:b891bb1b7642cde7c29173",
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
 });
 
 export default app;
